3781e409017c5d77efb57f8f492bba2a39a20380
Ke2777 <kirillegorov77@gmail.com>
2022-09-05 23:43:33 +0300

    ver 1.0

--- Counter/MainWindow.xaml.cs
+++ Counter/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Threading;
 
 namespace Counter
 {
@@ -22,50 +24,81 @@ namespace Counter
     /// </summary>
     public partial class MainWindow : Window
     {
-        int m_counter = 0, s_counter = 0, understand = 0, count = 0;
-        //int timerTick = 0;
 
+        float m_counter = 0, s_counter = 0, understand = 0;
+        float count = 0, speed = 0, record = 99999;
+        DateTime date;
+
+
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = this;
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
         }
 
         public void start_timer(object sender, RoutedEventArgs e)
         {
-            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
-
-            timer.Tick += new EventHandler(timerTick);
-            timer.Interval = new TimeSpan(0, 0, 1);
-            timer.Start();
+            dispatcherTimer.Start();
         }
 
-        private void timerTick(object sender, EventArgs e)
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
             s_counter ++;
-            if (s_counter == 60)
+            seconds.Text = s_counter.ToString();
+            if (s_counter > 59)
             {
                 m_counter++;
                 s_counter = 0;
             }
 
-            minute.Text = m_counter.ToString(); 
-            seconds.Text = s_counter.ToString();
+            minute.Text = m_counter.ToString();
+
         }
 
         private void btn_ponyatno_Click(object sender, RoutedEventArgs e)
         {
             understand++;
+            understand_box.Text = understand.ToString();
             if (m_counter != 0)
             {
                 count = m_counter / understand;
+                speed = understand / m_counter;
+                if (count < record)
+                {
+                    record = count;
+                }
+
+                record_box.Text = record.ToString();
+                count_box.Text = count.ToString();
+                speed_box.Text = speed.ToString();
             }
             
         }
 
         public void btn_stop_Click(object sender, RoutedEventArgs e)
         {
-            
+         dispatcherTimer.Stop();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            understand++;
+            understand_box.Text = understand.ToString();
+            if (m_counter != 0)
+            {
+                count = m_counter / understand;
+                speed = understand / m_counter;
+                if (count < record)
+                {
+                    record = count;
+                }
+
+                record_box.Text = record.ToString();
+                count_box.Text = count.ToString();
+                speed_box.Text = speed.ToString();
+            }
         }
 
     }
